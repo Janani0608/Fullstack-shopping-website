8728361b562c7ced2262714fb4f9e38957b12588
Janani <41537681+Janani0608@users.noreply.github.com>
2022-03-05 00:59:34 +0100

    Create app.js

---
+++ src/app/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient
+
+
+
+app.use(bodyparser.json());
+
+app.get('/shirts', (req, res) => {
+    req.header(headers)
+    MongoClient.connect('mongodb://0.0.0.0:27017', (err, client) => {
+        if (err) throw err
+        const db = client.db('shirt-shop')
+        db.collection('Shirts').find().toArray((err, result) => {
+            if (err) throw err
+            res.send(result);
+            console.log(result)
+        })
+    })
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => {`listening on ${port}`});

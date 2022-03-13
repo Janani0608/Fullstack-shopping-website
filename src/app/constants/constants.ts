7c120e45c5165c7f062f9505b0d37b5d22d1da5d
Janani <41537681+Janani0608@users.noreply.github.com>
2022-03-13 19:58:19 +0100

    Create constants.ts

---
+++ src/app/constants/constants.ts
@@ -0,0 +1,17 @@
+import { Shirt } from "../services/shirts.service";
+export const shirtsURL = "http://localhost:3000/shirts";
+export const emptyCart = {
+    cart: [] as Shirt[],
+    quantity: 0,
+    price: 0
+};
+export const emptyShirt = {
+    '_id': "",
+    'id': 0,
+    'colour': "",
+    'picture': "",
+    'price': 0,
+    'size': "",
+    'name': "",
+    'quantity': 0
+}
